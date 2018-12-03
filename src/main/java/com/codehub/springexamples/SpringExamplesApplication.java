30fb61d32bbae2fed9b6b826dfa2a7c9a9b65c4f
christos <chrisperis@gmail.com>
2018-12-04 01:48:22 +0200

    2_2_1-Spring-Bean-Annotations-Config-Scopes Adds example for Spring Bean Scopes

--- src/main/java/com/codehub/springexamples/SpringExamplesApplication.java
+++ src/main/java/com/codehub/springexamples/SpringExamplesApplication.java
@@ -1,6 +1,7 @@
 package com.codehub.springexamples;
 
 import com.codehub.springexamples.vehicle.Bike;
+import com.codehub.springexamples.vehicle.Car;
 import com.codehub.springexamples.vehicle.Vehicle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,34 @@ public class SpringExamplesApplication {
         SpringApplication.run(SpringExamplesApplication.class, args);
 
         ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-        Vehicle vehicle = context.getBean("bike", Bike.class);
-        vehicle.start();
+
+        Vehicle carBean = context.getBean("car", Car.class);
+        Vehicle anotherCarBean = context.getBean("car", Car.class);
+
+        //check equality
+        boolean singletonsCheckResult = (carBean == anotherCarBean);
+        System.out.println("Pointing to the same location: " + singletonsCheckResult);
+        System.out.println("Object location of carBean " + carBean);
+        System.out.println("Object location of anotherCarBean " + anotherCarBean);
+
+        //retrieve bean from spring container
+        Vehicle bikeBean = context.getBean("bike", Vehicle.class);
+
+        //retrieve the same bean and assign to a different object
+        Vehicle anotherBikeBean = context.getBean("bike", Vehicle.class);
+
+        //check equality
+        boolean prototypeCheckResult = (bikeBean == anotherBikeBean);
+        System.out.println("Pointing to the same location: " + prototypeCheckResult);
+        System.out.println("Object location of bikeBean " + bikeBean);
+        System.out.println("Object location of anotherBikeBean " + anotherBikeBean);
+
+        //call methods on the bean
+        carBean.start();
+        anotherCarBean.start();
+
+        bikeBean.start();
+        anotherBikeBean.start();
 
     }
 }
