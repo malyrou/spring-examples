e7f1f049580a82a220b31b1d1cfbc4775ea17e6a
christos <chrisperis@gmail.com>
2018-12-04 01:41:13 +0200

    2_1_1-Spring-Bean-XML-Config-Scopes Adds example for Spring Bean Scopes

--- src/main/java/com/codehub/springexamples/SpringExamplesApplication.java
+++ src/main/java/com/codehub/springexamples/SpringExamplesApplication.java
@@ -14,8 +14,34 @@ public class SpringExamplesApplication {
         SpringApplication.run(SpringExamplesApplication.class, args);
 
         ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-        Vehicle vehicle = context.getBean("car", Car.class);
-        vehicle.start();
+
+        Vehicle carBean = context.getBean("car", Car.class);
+        Vehicle anotherCarBean = context.getBean("car", Car.class);
+
+        //check equality
+        boolean singletonsCheckResult = (carBean == anotherCarBean);
+        System.out.println("Pointing to the same location: " + singletonsCheckResult);
+        System.out.println("Object location of carBean " + carBean);
+        System.out.println("Object location of anotherCarBean " + anotherCarBean);
+
+        //retrieve bean from spring container
+        Vehicle bikeBean = context.getBean("bike", Vehicle.class);
+
+        //retrieve the same bean and assign to a different object
+        Vehicle anotherBikeBean = context.getBean("bike", Vehicle.class);
+
+        //check equality
+        boolean prototypeCheckResult = (bikeBean == anotherBikeBean);
+        System.out.println("Pointing to the same location: " + prototypeCheckResult);
+        System.out.println("Object location of bikeBean " + bikeBean);
+        System.out.println("Object location of anotherBikeBean " + anotherBikeBean);
+
+        //call methods on the bean
+        carBean.start();
+        anotherCarBean.start();
+
+        bikeBean.start();
+        anotherBikeBean.start();
 
     }
 }
